25d836e172a9974f6cbd7dd323ae29fad7d457ef
Chaytan Inman <35582442+chaytanc@users.noreply.github.com>
2022-06-27 19:00:10 -0700

    Break post details trying to get createdAt to show up

---
+++ Instagram/Instagram/ViewControllers/DetailsNavController.h
@@ -0,0 +1,16 @@
+//
+//  DetailsNavController.h
+//  Instagram
+//
+//  Created by Chaytan Inman on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsNavController : UINavigationController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Instagram/Instagram/ViewControllers/DetailsNavController.m
@@ -0,0 +1,31 @@
+//
+//  DetailsNavController.m
+//  Instagram
+//
+//  Created by Chaytan Inman on 6/27/22.
+//
+
+#import "DetailsNavController.h"
+
+@interface DetailsNavController ()
+
+@end
+
+@implementation DetailsNavController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
